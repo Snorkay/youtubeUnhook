ade568af1713114bc18910f2f54d56664c508518
Snorkay <snork@protonmail.ch>
2018-06-24 18:14:18 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+YoutubeUnhook
+======
+A simple extension that replaces youtube url and embedded videos with HookTube alternative.
+
+## Version
+* Version 0.1.1
+
+**Extension only tested on Firefox 60**
+
+Special thanks to Swack for creating [Hooktube](https://hooktube.com) a great youtube "alternative".
+ 
+## Contact
+#### Snork
+* email: snork@protonmail.ch
+* Keybase: snork
+
---
+++ youtubeUnhook.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @names	YoutubeUnhook
+// @namespace	Snork
+// @description Replace embedded youtube frames with hooktube frames. Additionally redirect youtube URLS to hooktube.
+// @include	*
+// @version	0.1.1
+// ==/UserScript==
+
+
+//Autoplay Configs
+var autoplayLoad = 0;	//Default 0 - Set to 1 to enable autoplay on page load 
+
+//URL Replacement
+var url = window.location.toString();
+
+function replacer(yturl){
+	window.location = url.replace(yturl, 'hooktube.com');
+}
+
+if (url.indexOf('www.youtube.com') !== -1){
+	replacer(/youtube.com/);
+} else if (url.indexOf('www.youtu.be') !== -1){
+	replacer(/youtu.be/); 
+}
+
+//iFrame Replacement
+var watcher = new MutationObserver(interaction);
+watcher.observe(document,{childList:true,attributes:true,subtree:true});
+
+function interaction(){
+	auto(autoplayLoad);
+}
+
+function auto(autoplay){
+	var filter = Array.filter || Benchmark.filter;
+	var frames = document.getElementsByTagName("iframe");
+	frames = filter(frames, ytiFrames);
+
+	for(var i=0; i<frames.length; i++){
+		var frame = frames[i];
+		var src = frame.getAttribute('src');
+		var ht = src.replace('youtube', 'hooktube');
+		if(ht.indexOf('?') === -1){
+			ht += '?autoplay=' + autoplay;
+		} else{
+			ht += '&autoplay=' + autoplay;
+		}
+		frame.setAttribute('src', ht);
+	}
+}
+
+function ytiFrames(el){
+	if(el.hasAttribute('src')){
+		return el.getAttribute('src').indexOf('youtube') !== -1;
+	}
+	return false;
+}
+
+
+
+
+
